e81475f451c814efa955f42fc08ed2e5570485b2
khoicpct-collab <khoicpct@gmail.com>
2025-11-20 14:55:06 +0700

    Add liquids data with densities

---
+++ data/liquids.js
@@ -0,0 +1,7 @@
+const LIQUIDS = [
+    { id: 1, name: 'Dầu cá biển', density: 0.92 },
+    { id: 2, name: 'Dầu nành', density: 0.91 },
+    { id: 3, name: 'Dịch cá', density: 1.02 },
+    { id: 4, name: 'Nước mắm', density: 1.15 },
+    { id: 5, name: 'Mắm tôm', density: 1.18 }
+];
